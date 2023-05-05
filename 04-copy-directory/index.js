7775bd825d22a0b16d7996ca534122a7c0a31b18
shoxa <sllx.ok@gmail.com>
2023-05-05 23:29:46 +0500

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const path = require('path');
+const fs = require('fs');
 
+const srcFolderPath = path.join(__dirname, 'files');
+const dstFolderPath = path.join(__dirname, 'files-copy');
+
+
+fs.mkdir(dstFolderPath, { recursive: true }, (err) =>  {
+  if (err) {
+    throw err;
+  }
+});
+
+fs.readdir(srcFolderPath, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  
+  //copy files
+  data.forEach((fileName) => {
+    const srcFilePath = path.join(srcFolderPath, fileName);
+    const dstFilePath = path.join(dstFolderPath, fileName);
+    fs.copyFile(srcFilePath, dstFilePath, (err) => {
+      if (err) {
+        throw err;
+      }
+    })
+  })
+
+  //remove not existing files in dst folder
+  fs.readdir(dstFolderPath, (err, dstData) => {
+    if (err) {
+      throw err;
+    }
+    const last = dstData.filter((file) => !data.includes(file));
+    if (last.length) {
+      last.forEach((file) => {
+        const pathFile = path.join(dstFolderPath, file);
+        fs.rm(pathFile, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      })
+    }
+  });
+});
