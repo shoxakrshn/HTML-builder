7c4c2a571eb0e1246dda98c66b8d6dc0783a91c7
shoxa <sllx.ok@gmail.com>
2023-05-06 17:57:12 +0500

    refactor: insert code into function in task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,6 +4,39 @@ const fs = require('fs');
 const stylesPath = path.join(__dirname, 'styles');
 const cssBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
+const mergeStyles = (srcStyles, bundle) => {
+  fs.writeFile(bundle, '', (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  
+  fs.readdir(srcStyles, { withFileTypes: true }, (err, data) => {
+    if (err) {
+      throw err;
+    }
+  
+    const cssFiles = data.filter((file) => path.extname(file.name) === '.css' && !file.isDirectory());
+    const cssFilesPath = cssFiles.map((cssFile) => path.join(stylesPath, cssFile.name));
+    cssFilesPath.forEach((cssFilePath) => {
+      console.log(cssFilePath);
+      fs.readFile(cssFilePath, 'utf-8', (err, data) => {
+        if (err) {
+          throw err;
+        }
+        fs.appendFile(cssBundle, data, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    });
+  });
+};
+
+mergeStyles(stylesPath, cssBundle);
+
+/*
 fs.writeFile(cssBundle, '', (err) => {
   if (err) {
     throw err;
@@ -31,3 +64,4 @@ fs.readdir(stylesPath, { withFileTypes: true }, (err, data) => {
     });
   });
 });
+*/
