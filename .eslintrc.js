4a43298012e5b3c920ceaf7ea03931306fb79924
shoxa <sllx.ok@gmail.com>
2023-05-06 16:12:03 +0500

    refactor: add recursive check inner folder in task 04-copy-directory

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   'env': {
     'commonjs': true,
-    'es2022': true,
+    //'es2022': true,
     'node': true
   },
   'extends': 'eslint:recommended',
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,7 +4,64 @@ const fs = require('fs');
 const srcFolderPath = path.join(__dirname, 'files');
 const dstFolderPath = path.join(__dirname, 'files-copy');
 
+const remove = (dstDir, data) => {
+  fs.readdir(dstDir, (err, dstData) => {
+    if (err) {
+      throw err;
+    }
+    const last = dstData.filter((file) => !data.includes(file));
+    if (last.length) {
+      last.forEach((file) => {
+        const pathFile = path.join(dstDir, file);
+        fs.rm(pathFile, { force: true, recursive: true }, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    }
+  });
+};
 
+const copyDir = (srcDir, dstDir) => {
+  fs.mkdir(dstDir, { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+
+  fs.readdir(srcDir, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    //copy files
+    data.forEach((fileName) => {
+      const srcFilePath = path.join(srcDir, fileName);
+      const dstFilePath = path.join(dstDir, fileName);
+      fs.stat(srcFilePath, (err, dataStat) => {
+        if (err) {
+          throw err;
+        }
+        if (dataStat.isDirectory()) {
+          copyDir(srcFilePath, dstFilePath);
+        } else {
+          fs.copyFile(srcFilePath, dstFilePath, (err) => {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+      });
+    });
+    remove(dstDir, data);
+  });
+};
+
+
+copyDir(srcFolderPath, dstFolderPath);
+
+/*
 fs.mkdir(dstFolderPath, { recursive: true }, (err) =>  {
   if (err) {
     throw err;
@@ -45,3 +102,4 @@ fs.readdir(srcFolderPath, (err, data) => {
     }
   });
 });
+*/
