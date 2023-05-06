19039358c7b02dad2ad16088b20b7f34916c268c
shoxa <sllx.ok@gmail.com>
2023-05-07 00:00:53 +0500

    refactor: change to stream read and write

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,31 +5,16 @@ const stylesPath = path.join(__dirname, 'styles');
 const cssBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 const mergeStyles = (srcStyles, bundle) => {
-  fs.writeFile(bundle, '', (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-  
   fs.readdir(srcStyles, { withFileTypes: true }, (err, data) => {
     if (err) {
       throw err;
     }
-  
+    const write = fs.createWriteStream(bundle);
     const cssFiles = data.filter((file) => path.extname(file.name) === '.css' && !file.isDirectory());
     const cssFilesPath = cssFiles.map((cssFile) => path.join(stylesPath, cssFile.name));
     cssFilesPath.forEach((cssFilePath) => {
-      console.log(cssFilePath);
-      fs.readFile(cssFilePath, 'utf-8', (err, data) => {
-        if (err) {
-          throw err;
-        }
-        fs.appendFile(cssBundle, data, (err) => {
-          if (err) {
-            throw err;
-          }
-        });
-      });
+      const read = fs.createReadStream(cssFilePath, 'utf-8');
+      read.pipe(write);
     });
   });
 };
